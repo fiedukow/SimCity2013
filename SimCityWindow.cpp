ce34917ae8ec7a841e699c53cb62c253b33efd58
fiedukow <fiedukow@gmail.com>
2013-01-07 12:37:48 +0100

    Added StatusBars labels to SimCityWindow.

--- SimCityWindow.cpp
+++ SimCityWindow.cpp
@@ -1,11 +1,20 @@
 #include "SimCityWindow.h"
 #include "ui_SimCityWindow.h"
+#include <QLine>
 
 SimCityWindow::SimCityWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::SimCityWindow)
 {
   ui->setupUi(this);
+  fpsCounter_ = new QLabel("0");
+  speedCounter_ = new QLabel("x1");
+
+  statusBar()->addWidget(new QLabel("FPS: ", this));
+  statusBar()->addWidget(fpsCounter_);
+  statusBar()->addWidget(new QLabel("  |  ", this));
+  statusBar()->addWidget(new QLabel("Speed: ", this));
+  statusBar()->addWidget(speedCounter_);
 }
 
 SimCityWindow::~SimCityWindow()
--- SimCityWindow.h
+++ SimCityWindow.h
@@ -2,6 +2,7 @@
 #define SIMCITYWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
 
 namespace Ui {
 class SimCityWindow;
@@ -10,13 +11,15 @@ class SimCityWindow;
 class SimCityWindow : public QMainWindow
 {
   Q_OBJECT
-  
+
 public:
   explicit SimCityWindow(QWidget *parent = 0);
   ~SimCityWindow();
-  
+
 private:
   Ui::SimCityWindow *ui;
+  QLabel* fpsCounter_;
+  QLabel* speedCounter_;
 };
 
 #endif // SIMCITYWINDOW_H
