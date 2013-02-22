421a6ba04c1bbd13408b1fa1ad0a5e6bc513a794
fiedukow <fiedukow@gmail.com>
2013-02-22 16:32:13 +0100

    Added test printout to main file. Commented qt interface in main file. Fixed bad postgresql password in main file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,29 @@
 #include <QApplication>
 
+#include <iostream>
+
 #include "View/SimCityWindow.h"
 #include "Model/StaticBaseDriver.h"
 
+using namespace SimCity::Model;
+
 int main(int argc, char *argv[])
 {
-  QApplication a(argc, argv);
-  SimCityWindow w;
-  w.show();
+//  QApplication a(argc, argv);
+//  SimCityWindow w;
+//  w.show();
   /*
    * DBConnection
    */
   StaticBaseDriver dbDriver("dbname=simcity "
                             "user=simcity "
-                            "password=ssimcity");
+                            "password=simcity");
+  std::vector<StreetNode> nodes = dbDriver.getStreetNodes();
+  for(const StreetNode& node : nodes)
+    std::cout << node.nodeId << " "
+              << node.lat << " "
+              << node.lon << " "
+              << node.mos << std::endl;
 
-  return a.exec();
+//  return a.exec();
 }
