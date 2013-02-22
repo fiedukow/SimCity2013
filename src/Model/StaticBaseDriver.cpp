8f52e33b6c4255e0dc42d710835765b4cc17f8c6
fiedukow <fiedukow@gmail.com>
2013-02-22 16:29:51 +0100

    Added comments for StaticDatabaseDriver methods. Implemented getting streetNodes from postgresql.

--- src/Model/StaticBaseDriver.cpp
+++ src/Model/StaticBaseDriver.cpp
@@ -2,8 +2,14 @@
 #include "Exceptions.h"
 
 #include <iostream>
+#include <sstream>
 
 #include <pqxx/pqxx>
+
+namespace SimCity
+{
+namespace Model
+{
 
 StaticBaseDriver::StaticBaseDriver(const char* connectionDetails,
                                    bool persistent)
@@ -19,12 +25,35 @@ StaticBaseDriver::~StaticBaseDriver()
   disconnect();
 }
 
+std::vector<StreetNode> StaticBaseDriver::getStreetNodes()
+{
+  ensureDBConnection();
+  std::vector<StreetNode> result;
+  std::string query("SELECT * FROM StreetNodes");
+  pqxx::work t(*connection_);
+  pqxx::result nodes = t.exec(query);
+  for(const auto& node : nodes)
+    result.push_back(StreetNode(node));
+
+  disconnectIfNecessary();
+  return result;
+}
+
 bool StaticBaseDriver::ensureDBConnection()
 {
   if(connection_ != NULL)
     return false;
 
   connect();
+  return true;
+}
+
+bool StaticBaseDriver::disconnectIfNecessary()
+{
+  if(persistent_)
+    return false;
+
+  disconnect();
   return true;
 }
 
@@ -47,3 +76,6 @@ void StaticBaseDriver::disconnect()
   delete connection_;
   connection_ = NULL;
 }
+
+}//namespace Model
+}//namespace SimCity
--- src/Model/StaticBaseDriver.h
+++ src/Model/StaticBaseDriver.h
@@ -2,17 +2,41 @@
 #define STATICBASEDRIVER_H
 
 #include <string>
+#include <vector>
+
 #include <pqxx/pqxx>
 
+#include <Model/DBDataStructures.h>
+
+namespace SimCity
+{
+namespace Model
+{
+
+/**
+ * @brief Class used to communicate with postgresql database
+ *
+ * This class implements connection to posgresql and some basic
+ * db operation connected with this application.
+ * It allows to get any information are needed from SimCity official
+ * db structure.
+ *
+ * It uses pqxx library to communicate with postgresql
+ *
+ * @author Andrzej 'fiedukow' Fiedukowicz
+ */
 class StaticBaseDriver
 {
 public:
   StaticBaseDriver(const char* connectionDetails = "",
                    bool persistent = true);
   ~StaticBaseDriver();
+  std::vector<StreetNode> getStreetNodes();
 
 private:
-  /*
+  /**
+   * @brief ensures that class instantion has established connection to db
+   *
    * Connect to RDBMS if connection isn't already established
    * throws an exception if failed.
    *
@@ -22,6 +46,7 @@ private:
    *                           trying to connect to RDBMS
    */
   bool ensureDBConnection();
+  bool disconnectIfNecessary();
   void connect();
   void disconnect();
 
@@ -29,6 +54,11 @@ private:
   const std::string connectionDetails_;
   const bool persistent_;
   pqxx::connection* connection_;
+
+  friend int dupa();
 };
+
+}//namespace Model
+}//namespace SimCity
 
 #endif // STATICBASEDRIVER_H
