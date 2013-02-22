4059120df600c17d1d255e1b753c1932b06f6131
fiedukow <fiedukow@gmail.com>
2013-02-22 14:02:44 +0100

    Added postgresql connection params to main.

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,9 @@ int main(int argc, char *argv[])
   /*
    * DBConnection
    */
-  StaticBaseDriver dbDriver;
+  StaticBaseDriver dbDriver("dbname=simcity "
+                            "user=simcity "
+                            "password=ssimcity");
 
   return a.exec();
 }
