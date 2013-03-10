410c77120353e8297f0a0bf9b732dd1f06a46df5
fiedukow <fiedukow@gmail.com>
2013-03-10 17:54:01 +0100

    Implemented basic functionallity of World class. It loads map from DB and reacts for passing time by logining the message.

--- src/Model/World.cpp
+++ src/Model/World.cpp
@@ -1,5 +1,43 @@
 #include "World.h"
+#include <Common/GlobalLogger.h>
+#include <Model/StaticBaseDriver.h>
+#include <sstream>
 
-World::World()
+namespace SimCity
+{
+namespace Model
+{
+
+World::World(const std::string& dbName,
+             const std::string& dbUser,
+             const std::string& dbPassword)
+  : map_(readMapFromDB(dbName, dbUser, dbPassword))
 {
 }
+
+World::~World()
+{
+}
+
+void World::timePassed(uint ms)
+{
+  std::stringstream ss;
+  ss << "moving forward by " << ms << "ms.";
+  Common::globLog("NOT", "World", ss.str());
+  return;
+}
+
+Map World::readMapFromDB(const std::string& dbName,
+                         const std::string& dbUser,
+                         const std::string& dbPassword)
+{
+  StaticBaseDriver dbDriver(std::string("dbname=" + dbName +
+                                        " user=" + dbUser +
+                                        " password=" + dbPassword).c_str(),
+                            true);
+  return dbDriver.getMap();
+}
+
+}//namespace Model
+}//namespace SimCity
+
--- src/Model/World.h
+++ src/Model/World.h
@@ -1,10 +1,32 @@
 #ifndef WORLD_H
 #define WORLD_H
 
-class World
+#include <Model/SimulationPart.h>
+#include <Model/DBDataStructures.h>
+
+namespace SimCity
+{
+namespace Model
+{
+
+class World : public SimulationPart
 {
 public:
-  World();
+  World(const std::string& dbName,
+        const std::string& dbUser,
+        const std::string& dbPassword);
+  virtual ~World();
+  virtual void timePassed(uint ms);
+
+  static Map readMapFromDB(const std::string& dbName,
+                           const std::string& dbUser,
+                           const std::string& dbPassword);
+
+private:
+  Map map_;
 };
+
+}//namespace Model
+}//namespace SimCity
 
 #endif // WORLD_H
