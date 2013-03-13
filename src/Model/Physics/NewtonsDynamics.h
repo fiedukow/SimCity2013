7dc7bb57fa44587d973086afb2472dba9bc8102f
fiedukow <fiedukow@gmail.com>
2013-03-13 12:06:38 +0100

    Added Shift, TimeDuration and Velocity structures to Physics model.
    Added comment about units and physics laws used in Physics model.

--- src/Model/Physics/NewtonsDynamics.h
+++ src/Model/Physics/NewtonsDynamics.h
@@ -8,6 +8,31 @@ namespace Model
 namespace Physics
 {
 
+struct Mass;
+struct Acceleration;
+struct Force;
+struct TimeDuration;
+struct Shift;
+struct Velocity;
+
+
+/**
+ *  --------------------------------------
+ *  |     Phisical units used here       |
+ *  --------------------------------------
+ *  | Force          |   newtons         |
+ *  | Mass           |   kilograms       |
+ *  | Acceleration   |   meters/second^2 |
+ *  | Time duration  |   seconds         |
+ *  | Velocity       |   meter/second    |
+ *  --------------------------------------
+ *
+ *  Implemented :
+ *    F = ma
+ *    s = vt
+ *    v = at
+ */
+
 struct Vector3
 {
   Vector3 operator*(double scalar) const;
@@ -17,13 +42,10 @@ struct Vector3
   double x, y, z;
 };
 
-struct Mass;
-struct Acceleration;
-struct Force;
-
 struct Mass
 {
   Force operator*(const Acceleration& acc) const;
+
   double value;
 };
 
@@ -31,6 +53,7 @@ struct Force
 {
   Acceleration operator/(const Mass& mass) const;
   Mass operator/(const Acceleration& acc) const;
+
   Vector3 value;
 };
 
@@ -43,6 +66,32 @@ struct Acceleration
    * Multiplies acceleration vector by mass to get force.
    */
   Force operator*(const Mass& mass) const;
+  Velocity operator*(const TimeDuration& dt) const;
+
+  Vector3 value;
+};
+
+struct TimeDuration
+{
+  Shift operator*(const Velocity& dt) const;
+  Velocity operator*(const Acceleration& acc) const;
+
+  double value;
+};
+
+struct Velocity
+{
+  Shift operator*(const TimeDuration& dt) const;
+  TimeDuration operator/(const Acceleration& acc) const;
+
+  double value;
+};
+
+struct Shift
+{
+  Velocity operator/(const TimeDuration& dt) const;
+  TimeDuration operator/(const Velocity& dt) const;
+
   Vector3 value;
 };
 
