bb1dfe2acbf7575e5a7e89eaa7b1cc6fe53b5e08
fiedukow <fiedukow@gmail.com>
2013-03-13 12:06:18 +0100

    Added ObjectSnapshot class stub.

---
+++ src/Model/ObjectSnapshot.cpp
@@ -0,0 +1,13 @@
+#include "ObjectSnapshot.h"
+
+namespace SimCity2013
+{
+namespace Model
+{
+
+ObjectSnapshot::ObjectSnapshot()
+{
+}
+
+}//namespace SimCity2013
+}//namespace Model
---
+++ src/Model/ObjectSnapshot.h
@@ -0,0 +1,18 @@
+#ifndef OBJECTSNAPSHOT_H
+#define OBJECTSNAPSHOT_H
+
+namespace SimCity2013
+{
+namespace Model
+{
+
+class ObjectSnapshot
+{
+public:
+  ObjectSnapshot();
+};
+
+}//namespace SimCity2013
+}//namespace Model
+
+#endif // OBJECTSNAPSHOT_H
