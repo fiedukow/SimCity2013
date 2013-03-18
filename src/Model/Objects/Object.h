82999d2cf453b5adde9a3f8ea1558f41bf08aa41
fiedukow <fiedukow@gmail.com>
2013-03-18 14:24:12 +0100

    Added PlacedObject class stub.

--- src/Model/Objects/Object.h
+++ src/Model/Objects/Object.h
@@ -1,16 +1,21 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 #include <Model/Physics/NewtonsDynamics.h>
+#include <Model/Physics/Positioning.h>
+#include <memory>
 
 namespace SimCity
 {
 namespace Model
+{
+namespace Objects
 {
 
 class Object
 {
 public:
   Object();
+  virtual ~Object();
 
   virtual void getSnapshot() const = 0; //TODO it is no void returning
   virtual Physics::Mass getCurrentMass() const = 0;   //Argh...
@@ -19,6 +24,31 @@ public:
   virtual Physics::Force getCurrentForce() const = 0;
 };
 
+typedef std::shared_ptr<Object> ObjectPtr;
+
+class PlacedObject
+{
+public:
+  PlacedObject(const ObjectPtr& obj,
+               const Physics::Position& pos,
+               const Physics::Velocity& v);
+
+  ObjectPtr getObjectPtr() const;
+  Physics::Position getPosition() const;
+  Physics::Velocity getVelocity() const;
+  void dtMove(Physics::TimeDuration dt);
+
+private:
+  void dtUpdatePosition();
+  void dtUpdateVelocity();
+
+private:
+  ObjectPtr obj;
+  Physics::Position pos;
+  Physics::Velocity v;
+};
+
+}//namespace Objects
 }//namespace SimCity
 }//namespace Model
 
