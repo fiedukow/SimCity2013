94508a3be6cce736efd1ce2babfbe99752f4ab7f
fiedukow <fiedukow@gmail.com>
2013-04-17 09:11:01 +0200

    Added code for generating new objects in ObjectManager.

--- src/Model/ObjectManager.cpp
+++ src/Model/ObjectManager.cpp
@@ -1,5 +1,8 @@
 #include "ObjectManager.h"
 #include <Common/GlobalLogger.h>
+#include <Model/World.h>
+#include <Model/Objects/Objects.h>
+#include <memory>
 
 namespace SimCity
 {
@@ -16,14 +19,35 @@ ObjectManager::~ObjectManager()
 
 void ObjectManager::timePassed(uint ms)
 {
+  MapPtr map = world_->getMapSnapshot();
   while(objects_.size() < limit_)
   {
-  //  LiveObjectPtr newObject(new LiveObjectPtr)
-  //  objects_.push_back();
+    std::stringstream ss;
+    ss << "Adding new object: ";
+    StreetPtr startStreet = map->edges[rand()%map->edges.size()];
+    Car::Direction startDirection = rand()%2 ? Car::FS : Car::SF;
+    StreetNodePtr startNode = (startDirection == Car::FS
+                               ? startStreet->first
+                               : startStreet->second);
+    StreetNodePtr endNode = (startDirection == Car::FS
+                               ? startStreet->second
+                               : startStreet->first);
+    Physics::Position startPos(Physics::GeoCoords(startNode->lon.get(),
+                                                  startNode->lat.get()));
+    Physics::Position endPos(Physics::GeoCoords(endNode->lon.get(),
+                                                endNode->lat.get()));
+    Physics::Velocity startV((endPos-startPos)/Physics::TimeDuration(15.0));
+
+    LiveObjectPtr newObject(new Car(map,
+                                    startPos,
+                                    startV,
+                                    startStreet,
+                                    startDirection));
+    objects_.push_back(newObject);
+    world_->addObserver(std::dynamic_pointer_cast<Observer>(newObject));
+    world_->addPlacedObject(std::dynamic_pointer_cast<PlacedObject>(newObject));
+    Common::globLog("NOT", "ObjMgr", ss.str());
   }
-  std::stringstream ss;
-  ss << "moving forward by " << ms << "ms.";
-  Common::globLog("NOT", "ObjMgr", ss.str());
   return;
 }
 
