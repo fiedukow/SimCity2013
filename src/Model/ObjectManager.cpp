3cc51f61fceba8cdbf212bf53bdf0ccd5cccea4b
fiedukow <fiedukow@gmail.com>
2013-04-17 07:25:07 +0200

    Added  some stubs to ObjectManager class.

--- src/Model/ObjectManager.cpp
+++ src/Model/ObjectManager.cpp
@@ -6,16 +6,21 @@ namespace SimCity
 namespace Model
 {
 
-ObjectManager::ObjectManager()
-{
-}
+ObjectManager::ObjectManager(WorldPtr world, uint limit)
+  : world_(world),
+    limit_(limit)
+{}
 
 ObjectManager::~ObjectManager()
-{
-}
+{}
 
 void ObjectManager::timePassed(uint ms)
 {
+  while(objects_.size() < limit_)
+  {
+  //  LiveObjectPtr newObject(new LiveObjectPtr)
+  //  objects_.push_back();
+  }
   std::stringstream ss;
   ss << "moving forward by " << ms << "ms.";
   Common::globLog("NOT", "ObjMgr", ss.str());
--- src/Model/ObjectManager.h
+++ src/Model/ObjectManager.h
@@ -1,18 +1,28 @@
 #ifndef OBJECTMANAGER_H
 #define OBJECTMANAGER_H
 #include <Model/SimulationPart.h>
+#include <Model/Model.h>
+#include <list>
+#include <Model/Objects/LiveObject.h>
 
 namespace SimCity
 {
 namespace Model
 {
+using Objects::LiveObjectPtr;
+typedef std::list<LiveObjectPtr> LiveObjects;
 
 class ObjectManager : public SimulationPart
 {
 public:
-  ObjectManager();
+  ObjectManager(WorldPtr world, uint limit = 1000);
   virtual ~ObjectManager();
   virtual void timePassed(uint ms);
+
+private:
+  WorldPtr world_;
+  uint limit_;
+  LiveObjects objects_;
 };
 
 }//namespace SimCity
